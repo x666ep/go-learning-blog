7b00d44d53253569096d4ed4758e17133fbc2a96
x666ep <x666ep@gmail.com>
2022-04-15 10:41:52 +0300

    clean main func

--- cmd/go-learning-blog/main.go
+++ cmd/go-learning-blog/main.go
@@ -2,35 +2,32 @@ package main
 
 import (
 	"context"
+	"log"
+	"net"
+	"net/http"
+
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	service "github.com/x666ep/go-learning-blog/internal/app/go-learning-blog"
 	blog "github.com/x666ep/go-learning-blog/pkg/api/go-learning-blog/v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
-	"log"
-	"net"
-	"net/http"
 )
 
 func main() {
-	// Create a listener on TCP port
 	lis, err := net.Listen("tcp", ":7000")
 	if err != nil {
 		log.Fatalln("Failed to listen:", err)
 	}
 
-	// Create a gRPC server object
 	s := grpc.NewServer()
-	// Attach the Greeter service to the server
+
 	blog.RegisterGoLearningBlogServiceServer(s, service.NewGoLearningBlogApi())
-	// Serve gRPC server
+
 	log.Println("Serving gRPC on 0.0.0.0:7000")
 	go func() {
 		log.Fatalln(s.Serve(lis))
 	}()
 
-	// Create a client connection to the gRPC server we just started
-	// This is where the gRPC-Gateway proxies the requests
 	conn, err := grpc.DialContext(
 		context.Background(),
 		"0.0.0.0:7000",
@@ -42,7 +39,7 @@ func main() {
 	}
 
 	gwmux := runtime.NewServeMux()
-	// Register Greeter
+
 	err = blog.RegisterGoLearningBlogServiceHandler(context.Background(), gwmux, conn)
 	if err != nil {
 		log.Fatalln("Failed to register gateway:", err)
